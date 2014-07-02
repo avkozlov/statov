756fb5e920cb90663d30f25437580fe83508b79e
anton <kozlov84@gmail.com>
2014-07-02 22:00:59 +0600

    Deploy

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os.path
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
@@ -7,7 +9,7 @@ import codecs
 import numpy as np
 from pandas import read_csv, DataFrame, ExcelFile
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 from myproject.myapp.models import Document
 from myproject.myapp.forms import DocumentForm
@@ -36,17 +38,27 @@ def list(request):
     a = Document.objects.last()
     url = 'myproject' + a.docfile.url
 
-    v = ExcelFile(url).parse("Sheet1", parse_cols=[0, 18, 26, 25, 23])
-    v.columns = ['Request','Product', 'Paid', 'PaidDate', 'Type']
-    # it is right!.to_datetime,
-    v.PaidDate = pd.to_datetime(v.PaidDate, format='%d.%m.%Y' )
-    df = pd.pivot_table(v, values='Paid', rows='PaidDate', cols=['Type', 'Product'], aggfunc=[np.sum, np.count_nonzero])
-    df = df.resample('M', how='sum')
-    df = df.fillna(value=0)
+    if os.path.isfile(url):
+        vic = 'TRUE'
 
-    # Render list page with the documents and the form
-    return render_to_response(
-        'myapp/list.html',
-        {'documents': documents, 'form': form, 'df': df.to_html(classes="table-condensed"),'url':url},
-        context_instance=RequestContext(request)
-    )
+        v = ExcelFile(url).parse("Sheet1", parse_cols=[0, 18, 26, 25, 23])
+        v.columns = ['Request','Product', 'Paid', 'PaidDate', 'Type']
+        # it is right!.to_datetime,
+        v.PaidDate = pd.to_datetime(v.PaidDate, format='%d.%m.%Y' )
+        df = pd.pivot_table(v, values='Paid', rows='PaidDate', cols=['Type', 'Product'], aggfunc=[np.sum, np.count_nonzero])
+        df = df.resample('M', how='sum')
+        df = df.fillna(value=0)
+
+        # Render list page with the documents and the form
+        return render_to_response(
+            'myapp/list.html',
+            {'documents': documents, 'form': form, 'df': df.to_html(classes="table-condensed"),'url':url, 'vic': vic},
+            context_instance=RequestContext(request)
+        )
+    else:
+        vic = 'False'
+        return render_to_response(
+            'myapp/list.html',
+            {'documents': documents, 'form': form, 'url': url, 'vic': vic},
+            context_instance=RequestContext(request)
+        )
